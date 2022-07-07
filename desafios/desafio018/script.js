4444f2bae2e2da4078bc75e63be58910f6f0a97f
Danilo Vicentin. S <98027715+Danilo-Vicentin-Silva@users.noreply.github.com>
2022-07-07 17:21:37 -0300

    finalização desafio018

---
+++ desafios/desafio018/script.js
@@ -0,0 +1,29 @@
+const body = document.querySelector('body')
+
+function pintaVermelho(params) {
+    body.style.backgroundColor = 'Red'
+}
+
+function pintaAzul(params) {
+    body.style.backgroundColor = 'Blue'
+}
+
+function pintaAmarelo(params) {
+    body.style.backgroundColor = 'Yellow'
+}
+
+function pintaLaranja(params) {
+    body.style.backgroundColor = 'Orange'
+}
+
+function pintaRoxo(params) {
+    body.style.backgroundColor = 'Purple'
+}
+
+function pintaVerde(params) {
+    body.style.backgroundColor = 'Green'
+}
+
+function limpaTela (params) {
+    body.style.backgroundColor = 'white'
+} 
