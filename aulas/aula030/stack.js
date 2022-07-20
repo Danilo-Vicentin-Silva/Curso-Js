27de8b836e30ee5e75c2b0004e5a07c59a74e9dd
Danilo Vicentin.S <danilovicentindasilva@gmail.com>
2022-07-20 08:27:53 -0300

    Estudo de Stacks

--- aulas/aula030/stack.js
+++ aulas/aula030/stack.js
@@ -1,7 +1,9 @@
+/*
+
 class Stack {
     constructor() {
-        this.count = 0
-        this.items = {}
+        this._count = 0
+        this._items = {}
     }
     push(element) {
         this.items[this.count] = element
@@ -13,8 +15,52 @@ class Stack {
     isEmpty() {
         return this.count === 0
     }
+    pop() {
+        if (this.isEmpty()) {
+            return undefined
+        }
+        this.count--
+        const result = this.items[this.count]
+        delete this.items[this.count]
+        return result
+    }
+    peek() {
+        if (this.isEmpty()) {
+            return undefined
+        }
+        return this.items[this.items - 1]
+    }
+    clear() {
+        this.items = {}
+        this.count = 0
+    } // Ou while(!this.isEmpty()) {this.pop} // Para remover um por um, assim respeitando o LIFO
+    toString() {
+        if (this.isEmpty()) {
+            return ''
+        }
+        let objString = `${this.items[0]}`
+        for (let i = 1; i < this.count; i++) {
+            objString = `${objString},${this.items[i]}`
+        }
+        return objString
+    }
 }
 
-const stack = new Stack()
-stack.push(5)
-stack.push(8)
+*/
+
+const items = new WeakMap()
+class Stack {
+    constructor() {
+        items.set(this, [])
+    }
+    push(element) {
+        const s = items.get(this)
+        s.push(element)
+    }
+    pop() {
+        const s = items.get(this)
+        const r = s.pop()
+        return r
+    }
+}
+
