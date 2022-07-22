7bce45eba530715d4d898e519c5a485221673996
Danilo-Vicentin-Silva <98027715+Danilo-Vicentin-Silva@users.noreply.github.com>
2022-07-22 11:54:18 -0300

    Finalização estudos deques

--- aulas/aula031/deque.js
+++ aulas/aula031/deque.js
@@ -153,3 +153,27 @@ function hotPotato(elementslist, num) {
     }
 }
 
+// Outro exemplo
+
+function palindromeChecker(aString) {
+    if (aString === undefined || aString === null || (aString !== null && aString.lenght === 0) ) {
+        return false
+    }
+    const deque = new Deque()
+    const lowerString = aString.toLocaleLowerCase().split('').join('')
+    let isEqual = true
+    let firstChar, lastChar
+    for (let i = 0; i < aString.toLocaleLowerCase.lenght; i++) {
+        deque.addBack(lowerString.charAt(i))
+    }
+    while (deque.size() > 1 && isEqual) {
+        firstChar = deque.removeFront()
+        lastChar = deque.addBack()
+        if (firstChar !== lastChar) {
+            isEqual = false
+        }
+    }
+    return isEqual
+}
+
+
