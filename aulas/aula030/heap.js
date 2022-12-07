a37592e3e963fceceb5d519529601baa87c5dcda
Danilo <danilovicentindasilva@gmail.com>
2022-12-07 16:44:40 -0300

    Estudos de Heaps

--- aulas/aula030/heap.js
+++ aulas/aula030/heap.js
@@ -9,22 +9,11 @@ function defaultCompare(a, b) {
 	}
 	return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN
 }
-//Para a ECMANScript antiga:
-/*function swap(array, a, b) {
-    const temp = array[a]
-    array[a] = array[b]
-    array[b] = temp
-}
-*/
-//Para a nova ECMANScript (desde 2015):
 const swap = (array, a, b) => [array[a], array[b]] = [array[b], array[a]]
 
-//Heap Binário
-//Estrutura de dados feita para se extrair valores minímos e máximos o mais rapido possível
-
 class MinHeap {
-    constructor(compareFn = defaultCompare) {
-        this.compareFn = compareFn
+    constructor(CompareFn = defaultCompare) {
+        this.CompareFn = CompareFn
         this.heap = []
     }
     getLeftIndex(index) {
@@ -50,13 +39,55 @@ class MinHeap {
     siftUp(index) {
         let parent = this.getParentIndex(index)
         while (
-            index > 0 
-            &&
-            this.compareFn(this.heap[parent], this.heap[index]) > Compare.BIGGER_THAN
+            index > 0 &&
+            this.CompareFn(this.heap[parent], this.heap[index]) > Compare.BIGGER_THAN
         ) {
             swap(this.heap, parent, index)
             index = parent
             parent = this.getParentIndex(index)
         }
+    }
+    size() {
+        return this.heap.length
+    }
+    isEmpty() {
+        return this.size() === 0
+    }
+    findMinimum() {
+        return this.isEmpty() ? undefined : this.heap[0]
+    }
+    extract() {
+        if (this.isEmpty()) {
+            return undefined
+        }
+        if (this.size() === 1) {
+            return this.heap.shift()
+        }
+        const removedValue = this.heap.shift()
+        this.siftDown(0)
+        return removedValue
+    }
+    siftDown(index) {
+        let element = index
+        const left = this.getLeftIndex(index)
+        const right = this.getRightIndex(index)
+        const size = this.size()
+        if (
+            left < size &&
+            this.CompareFn(this.heap[element], this.heap[left]) > Compare.BIGGER_THAN
+        ) {
+            element = left
+        }
+        if (
+            right < size &&
+            this.CompareFn(this.heap[element], this.heap[right]) > Compare.BIGGER_THAN
+        ) {
+            element = right
+        }
+        if (index !== element) {
+            swap(this.heap, index, element) {
+                this.siftDown(element)
+            }
+        } 
     }
 }
